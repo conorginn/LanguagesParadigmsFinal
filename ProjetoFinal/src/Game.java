cc933b10609e911258e9b25ac278949ab97e5ac3
Matthew-Riddell <d00245674@student.dkit.ie>
2024-05-25 23:32:04 +0100

    OnlineGame

--- ProjetoFinal/src/Game.java
+++ ProjetoFinal/src/Game.java
@@ -2,23 +2,27 @@
 
 public class Game extends Item {
     private String developer;
+    private String platform;
 
     // Default constructor
     public Game() {
         super();
         this.developer = "";
+        this.platform = "";
     }
 
     // Parameterized constructor
-    public Game(String title, String developer) {
+    public Game(String title, String developer, String platform) {
         super(title);
         this.developer = developer;
+        this.platform = platform;
     }
 
     // Copy constructor
     public Game(Game other) {
         super(other);
         this.developer = other.developer;
+        this.platform = other.platform;
     }
 
     // Getters and setters
@@ -26,14 +30,22 @@ public class Game extends Item {
         return developer;
     }
 
+    public String getPlatform() {
+        return platform;
+    }
+
     public void setDeveloper(String developer) {
         this.developer = developer;
+    }
+
+    public void setPlatform(String platform) {
+        this.platform = platform;
     }
 
     // Overridden print method
     @Override
     public void print() {
         super.print();
-        System.out.println("developer: " + developer);
+        System.out.println("developer: " + developer + "platform: " + platform);
     }
 }
--- ProjetoFinal/src/OnlineGame.java
+++ ProjetoFinal/src/OnlineGame.java
@@ -1,4 +1,34 @@
 // OnlineGame SubClass of Game
 
 public class OnlineGame extends Game {
+    private String downloadLink;
+
+    // Default constructor
+    public OnlineGame() {
+        super();
+        this.downloadLink = "";
+    }
+
+    // Parameterized constructor
+    public OnlineGame(String title, String developer, String platform, String downloadLink) {
+        super(title, developer, platform);
+        this.downloadLink = downloadLink;
+    }
+
+    // Copy constructor
+    public OnlineGame(OnlineGame other) {
+        super(other);
+        this.downloadLink = other.downloadLink;
+    }
+
+    // Getters and setters
+    public String getDownloadLink() { return downloadLink; }
+    public void setDownloadLink(String downloadLink) { this.downloadLink = downloadLink; }
+
+    // Overridden print method
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Download Link: " + downloadLink);
+    }
 }
